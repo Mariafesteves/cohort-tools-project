fc44000c0361a4b63a023a890352b925db024841
Mariaesteves <mariaferraesteves@hotmail.com>
2025-05-13 16:31:29 +0100

    add mongoose- schemas

--- server/app.js
+++ server/app.js
@@ -5,12 +5,19 @@ const cors = require("cors");
 
 const PORT = 5005;
 
+const mongoose = require("mongoose");
+ 
+mongoose
+.connect("mongodb://127.0.0.1:27017/cohort-tools-api") // Corrigido o endereço e a porta
+.then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+.catch(err => console.error("Error connecting to MongoDB", err));
+
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
+const Cohort = require("./models/cohortModel");
+const Student = require("./models/studentsModel");
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -37,11 +44,13 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", function (req, res, next){
-  res.json(cohorts);
+  
+  res.send("hello")
+  
 })
 
 app.get("/api/students", function (req, res, next){
-  res.json(students);
+  res.json(Student);
 })
 
 
